75da8cce52fd3911e5cb5f0d23843e438bf3bd4a
Kvnalx <19102293@ittelkom-pwt.ac.id>
2023-06-08 17:28:53 +0700

    Only Adding The AI Model Left

--- app/src/main/java/com/myscan/goscan/ui/add_item/AddGroceriesListActivity.kt
+++ app/src/main/java/com/myscan/goscan/ui/add_item/AddGroceriesListActivity.kt
@@ -5,30 +5,41 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.lifecycle.ViewModelProvider
+import com.myscan.goscan.R
+import com.myscan.goscan.data.item_class.GroceriesItem
 import com.myscan.goscan.databinding.ActivityAddGroceriesListBinding
-import com.myscan.goscan.ml.ModelCnnConverted
-import com.myscan.goscan.ml.ModelMnv2Converted
 import com.myscan.goscan.ml.ModelMnv2V2Converted
-import com.myscan.goscan.ui.camera.CameraActivity
-import com.myscan.goscan.utils.rotateFile
+import com.myscan.goscan.ui.main.MainActivity
+import com.myscan.goscan.ui.view_model.GroceriesViewModel
+import com.myscan.goscan.utils.createCustomTempFile
 import org.tensorflow.lite.support.image.TensorImage
 import java.io.File
 
 class AddGroceriesListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddGroceriesListBinding
+    private lateinit var groceriesViewModel: GroceriesViewModel
     private var getFile: File? = null
+    private lateinit var photoPathFile: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddGroceriesListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        groceriesViewModel = ViewModelProvider(this)[GroceriesViewModel::class.java]
 
         if (!allPermissionGranted()) {
             ActivityCompat.requestPermissions(
@@ -36,7 +47,41 @@ class AddGroceriesListActivity : AppCompatActivity() {
             )
         }
 
-        binding.btnCamera.setOnClickListener { intentCameraX() }
+        binding.btnCamera.setOnClickListener { cameraIntent() }
+
+
+        binding.fabAddGroceriesItems.isEnabled = false
+
+        binding.tiBrand.addTextChangedListener(addTextWatcher)
+        binding.tiPrice.addTextChangedListener(addTextWatcher)
+        binding.tiQuantity.addTextChangedListener(addTextWatcher)
+
+        binding.fabAddGroceriesItems.setOnClickListener {
+            val name = binding.tiBrand.text.toString()
+            val price = binding.tiPrice.text.toString().toInt()
+            val total = binding.tiQuantity.text.toString().toInt()
+            val totalPrices = price * total
+            val images = getFile?.absolutePath ?: ""
+
+            val groceriesItems = GroceriesItem(name, totalPrices, total, images)
+            groceriesViewModel.addGroceriesItem(groceriesItems,
+                success = {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.success_adding_groceries),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                },
+                failed = {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.failed_adding_groceries),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -47,34 +92,40 @@ class AddGroceriesListActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (!allPermissionGranted()) {
-                Toast.makeText(this, "Camera permission is denied", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.no_permission), Toast.LENGTH_SHORT).show()
                 finish()
             }
         }
     }
 
-    private val launcherIntentCameraX = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == CAMERA_X_RESULT) {
-            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                it.data?.getSerializableExtra("picture", File::class.java)
-            } else {
-                it.data?.getSerializableExtra("picture")
-            } as? File
+    private fun cameraIntent() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
 
-            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@AddGroceriesListActivity,
+                "com.myscan.goscan",
+                it
+            )
+            photoPathFile = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launchCamera.launch(intent)
 
-            myFile?.let { file ->
-                rotateFile(file, isBackCamera)
-                getFile = file
-                binding.ivImageHolder.setImageBitmap(BitmapFactory.decodeFile(file.path))
-            }
         }
     }
 
-    private fun intentCameraX() =
-        launcherIntentCameraX.launch(Intent(this, CameraActivity::class.java))
+    private val launchCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val photoFiles = File(photoPathFile)
+            photoFiles.let { files ->
+                getFile = files
+                binding.ivImageHolder.setImageBitmap(BitmapFactory.decodeFile(files.path))
+            }
+        }
+    }
 
     private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
@@ -85,7 +136,21 @@ class AddGroceriesListActivity : AppCompatActivity() {
 
         val newestBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
         val tensorImage = TensorImage.fromBitmap(newestBitmap)
-        
+
+    }
+
+    private val addTextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            binding.fabAddGroceriesItems.isEnabled = listOf(
+                !binding.tiBrand.text.isNullOrEmpty(),
+                !binding.tiPrice.text.isNullOrBlank(),
+                !binding.tiQuantity.text.isNullOrEmpty()
+            ).all { it }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
     }
 
     companion object {
